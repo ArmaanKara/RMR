2fd2605d7f0dc8305f0d8c46d5ac430fb9e876ef
armaankara <vancouverarmaan@gmail.com>
2021-10-18 14:55:24 -0700

    first commit to new repo

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import Navbar from './Components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'; 
+import './App.css';
+import Home from './Components/pages/Home'
+import signUp from './Components/pages/signup';
+import Residences from './Components/pages/Residences';
+import contactUs from './Components/pages/contactUs';
+
+
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar/>
+        <Switch>
+          <Route path='/residences' component={Residences}/>
+          <Route path='/' exact component={Home}/>
+          <Route path='/contactus' component={contactUs}/>
+          <Route path='/sign-up' component={signUp}/>
+        </Switch>
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/HeroSection.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import '../App.css';
+import { Button } from './Button';
+import './HeroSection.css';
+import { SearchBar } from './SearchBar';
+import Cards from './Cards';
+
+class HeroSection extends React.Component {
+    
+    state = {
+        search : ""
+    }
+
+    onChange = (e) =>{
+        this.setState({ search : e.target.value });
+    }
+    render() {
+
+        return(
+            <div className='hero-container'>
+                {/* <img src="/images/amy-tran-NbP0_9xb83s-unsplash.jpg" /> */}
+                <h1>Rate My Residence</h1>
+                <p>Find a Residence Below!</p>
+                {/* <input type="text" icon="search" label="Search...." onChange={this.onChange}/> */}
+                <div className="hero-btns">
+                    <Button className='btns' buttonStyle='btn--outline'
+                    buttonSize='btn--large'>Write a review</Button>
+                    <Button className='btns' buttonStyle='btn--primary'
+                    buttonSize='btn--large'>Residences <i className='fas fa-house-user'/> </Button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default HeroSection;
